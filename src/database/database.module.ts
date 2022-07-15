c9202dc4d44cb0aa9ed71eafdd715de7424a448f
lourivanrluz <lourivan-rodrigues@hotmail.com>
2022-07-14 22:48:30 -0300

    feature:deploy in progress

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -9,11 +9,7 @@ import { GenresEntity } from 'src/app/films/entites/genres.entity';
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.POSTGRES_HOST,
-      port: Number(process.env.POSTGRES_PORT),
-      username: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DB,
+      url: process.env.DATABASE_URL,
       synchronize: true,
       entities: [FilmsEntity, GenresEntity],
       //__dirname + '/**/*.entity{.js,.ts}'
